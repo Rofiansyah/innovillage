93a9b0680f97fdf9a690e0cd9517d5014203b0df
Rofiansyah <wizmanrofiansyah@gmail.com>
2022-11-21 20:26:35 +0700

    V1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 80;
 const staticpath = path.join(__dirname, "/");
 
 var antares = require("antares-http");
