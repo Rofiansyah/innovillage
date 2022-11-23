5c8f030c110e8f6ea8038c96d5c535b862348b0e
Rofiansyah <wizmanrofiansyah@gmail.com>
2022-11-24 01:38:36 +0700

    V3_PENYEMPURNAAN

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 setInterval(function () {
-    fetch("https://innovillage2022.herokuapp.com/api")
+    fetch("http://localhost:8080/api")
+    //fetch("https://innovillage2022.herokuapp.com/api")
       .then((hasil) => hasil.json())
       .then((res) => {
         console.log(res);
@@ -7,5 +8,6 @@ setInterval(function () {
         document.getElementById("kel_udara").innerHTML = res.kelUdara;
         document.getElementById("kel_tanah").innerHTML = res.kelTanah;
         document.getElementById("ph").innerHTML = res.phTanah;
+        document.getElementById("indikator").innerHTML = res.indikator;
       });
   }, 1000);
